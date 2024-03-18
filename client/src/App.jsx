de91969e48e2311ace839a13f6cb048f4ada53d9
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-03-18 11:39:23 +0530

    creating the home page UI and UX

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {BrowserRouter as Router , Routes ,Route} from 'react-router-dom';
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantsPage from './routes/RestaurantsPage';
+
 
 const App =( )=>{
-    return <div>App</div>
-}
+    return (
+    <div className='container'>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route exact path="/restaurants/:id/update" element={<UpdatePage/>} />
+                <Route exact path="/restaurants/:id" element={<RestaurantsPage/>} />
+            </Routes>
+        </Router>
+    </div>
+)}
 
 export default  App;
