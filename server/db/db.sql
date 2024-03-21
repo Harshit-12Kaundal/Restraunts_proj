ff85ae82bce938cc4e8f237d9b4cb916205cac04
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-03-21 11:36:57 +0530

    added comment into review table

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE review (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL REFERENCES restraunts(id),
+    restaurant_id BIGINT NOT NULL REFERENCES restraunts(id),  -- forign key added t o join two tables
     name VARCHAR(50) NOT NULL,
     review TEXT NOT NULL,
     rating INT NOT NULL CHECK (rating>=1 AND rating<=5)
